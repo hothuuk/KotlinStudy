81418885735a4afef07a80123854ec97d2873134
hothuuk <s22014@gsm.hs.kr>
2024-01-30 15:02:58 +0900

    15664

---
+++ src/main/kotlin/y2024m1/week5/15664.kt
@@ -0,0 +1,38 @@
+package y2024m1.week5
+
+fun main() {
+    val (n, m) = readln().split(" ").map { it.toInt() }
+    val nums = readln().split(" ").map { it.toInt() }
+    val nums2 = nums.sorted().toIntArray()
+
+    val check = Array(n) {false}
+    val set = HashSet<String> ()
+    val arr = IntArray(m)
+    val sb = StringBuilder()
+
+    fun find(len: Int, idx: Int) {
+        if (len == m) {
+            val tmp = StringBuilder()
+            arr.forEach { tmp.append(it).append(' ') }
+
+            if (!set.contains(tmp.toString())) {
+                sb.append(tmp).append('\n')
+                set.add(tmp.toString())
+            }
+
+            return
+        }
+
+        for (i in idx until n) {
+            if (!check[i]) {
+                check[i] = true
+                arr[len] = nums2[i]
+                find(len + 1, i)
+                check[i] = false
+            }
+        }
+    }
+
+    find(0, 0)
+    print(sb)
+}
