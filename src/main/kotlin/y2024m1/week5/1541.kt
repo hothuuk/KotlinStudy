35fca059ab55cb972b67913d733b213507aa7e20
hothuuk <s22014@gsm.hs.kr>
2024-01-31 16:28:51 +0900

    1541

---
+++ src/main/kotlin/y2024m1/week5/1541.kt
@@ -0,0 +1,12 @@
+package y2024m1.week5
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val str = readLine().split("-")
+    var min = str[0].split("+").sumOf { it.toInt() }
+
+    for (i in 1 until str.size) {
+        min -= str[i].split("+").sumOf { it.toInt() }
+    }
+
+    println(min)
+}
