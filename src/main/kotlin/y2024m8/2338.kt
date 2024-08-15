499e9b734b80e272d096be414cecdfd2f149d260
hothuuk <kijkj123@gmail.com>
2024-08-15 23:39:27 +0900

    2024.08.15

---
+++ src/main/kotlin/y2024m8/2338.kt
@@ -0,0 +1,12 @@
+package y2024m8
+
+import java.math.BigInteger
+
+fun main() {
+    val a = BigInteger(readln())
+    val b = BigInteger(readln())
+
+    println(a.add(b))
+    println(a.minus(b))
+    println(a.multiply(b))
+}
