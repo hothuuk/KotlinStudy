d8fd2a163025744d295e92f1ee87c02f633b7e00
hothuuk <kijkj123@gmail.com>
2024-10-02 19:29:44 +0900

    2024.10.02

---
+++ src/main/kotlin/y2024m10/6840.kt
@@ -0,0 +1,3 @@
+package y2024m10
+
+fun main()=print(Array(3){readln().toInt()}.sorted()[1])
---
+++ src/main/kotlin/y2024m10/7891.kt
@@ -0,0 +1,3 @@
+package y2024m10
+
+fun main()=repeat(readln().toInt()){println(readln().split(" ").sumOf{it.toInt()})}
