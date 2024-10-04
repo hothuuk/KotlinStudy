6612c26cff95fd6c4cc56f694795b8d262c09337
hothuuk <kijkj123@gmail.com>
2024-10-04 18:53:32 +0900

    2024.10.04

---
+++ src/main/kotlin/y2024m10/8370.kt
@@ -0,0 +1,3 @@
+package y2024m10
+
+fun main(){val(a,b,c,d)=readln().split(" ").map{it.toInt()};print(a*b+c*d)}
---
+++ src/main/kotlin/y2024m10/8437.kt
@@ -0,0 +1,3 @@
+package y2024m10
+
+fun main(){val a=List(2){readln().toBigInteger()};val c=2.toBigInteger();println((a[0]+a[1])/c);print((a[0]-a[1])/c)}
