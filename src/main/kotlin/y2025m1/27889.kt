f89e3af3891b5e3c22bf8e74dfd7b7e0f2847b55
hothuuk <kijkj123@gmail.com>
2025-01-01 16:07:03 +0900

    2025.01.01

---
+++ src/main/kotlin/y2025m1/27889.kt
@@ -0,0 +1,12 @@
+package y2025m1
+
+fun main() {
+    val s = readln()
+
+    when(s) {
+        "NLCS" -> print("North London Collegiate School")
+        "BHA" -> print("Branksome Hall Asia")
+        "KIS" -> print("Korea International School")
+        "SJA" -> print("St. Johnsbury Academy")
+    }
+}
